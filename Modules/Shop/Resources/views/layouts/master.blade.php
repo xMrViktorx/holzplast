a9fe12b71649618818337ae8ae7e4d6cc2ee56fe
Molnár Viktor <molviktor99@gmail.com>
2023-11-01 20:10:49 +0100

    Shop module

---
+++ Modules/Shop/Resources/views/layouts/master.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Holz-Plast Shop</title>
+    <link rel="icon" href="{{ url('build/images/icon.ico') }}" type="image/x-icon" />
+    <link rel="shortcut icon" href="{{ url('build/images/icon.ico') }}" type="image/x-icon" />
+    @vite('resources/css/app.css')
+</head>
+
+<body style="background-color: #CEF3AE">
+    @yield('content')
+
+</body>
+
+</html>
