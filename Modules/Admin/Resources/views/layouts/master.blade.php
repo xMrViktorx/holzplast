41c3afeb51d7978e14e00957611ddc138d19f59c
Molnár Viktor <molviktor99@gmail.com>
2023-11-13 20:12:11 +0100

    Pagination margin fix

--- Modules/Admin/Resources/views/layouts/master.blade.php
+++ Modules/Admin/Resources/views/layouts/master.blade.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="robots" content="noindex">
     <title>Holz-Plast Admin</title>
     <link rel="icon" href="{{ url('build/images/icon.ico') }}" type="image/x-icon" />
     <link rel="shortcut icon" href="{{ url('build/images/icon.ico') }}" type="image/x-icon" />
